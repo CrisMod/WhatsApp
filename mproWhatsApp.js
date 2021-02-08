2852dde977e1bf9285aadad3bc7397964fb239ca
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-02-07 20:57:03 -0500

    Update mproWhatsApp.js

--- mproWhatsApp.js
+++ mproWhatsApp.js
@@ -1,22 +1,135 @@
-{
-	"version": "13.2",
-	"download": "https://crismod.ml/mpro.html",
-	"update": [
-		{
-			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-es.html"
-		},
-		{
-			"languaje": "en",
-			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html"
-		},
-		{
-			"languaje": "pt-BR",
-			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
-		},
-		{
-			"languaje": "pt",
-			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<style>
+		html, body {
+			background-color: #FFFFFF; 
+			color: #303030;
 		}
-	]
-}
+		body {
+			padding-top: 0.5%;
+			padding-left: 2%;
+			padding-right: 2%;
+			font-size: 14px;
+			font-family: 'Roboto', 'Helvetica Neue', sans-serif;
+		}
+		.title {
+			display: block;
+			width: 100%;
+			text-align: center;
+			font-size: 16px;
+		}
+		p {
+			line-height: 1.7;
+			display: block;
+		}
+		b { 
+			font-size: large;
+		}
+		h2 {
+			color: #2b2b2b;
+			float: left;
+			line-height: 0;
+			display: block;			
+		}
+		date {
+			line-height: 3;
+			font-family: monospace;
+			font-size: 12px;
+			font-style: italic;
+			float: right;
+			color: green;
+		}
+		wbase {
+			background-color: #2b2b2b;
+			color: #ffffff;
+			font-weight: bold;
+		}
+		wbase, rad {
+			padding: .2rem .7rem; border-radius: .0rem;
+		}
+		
+		new, fix, ex {
+			padding: .2rem .7rem; border-radius: 3.0rem;
+			}
+		new { 
+			color: #4f6e33;
+			background-color: #e9f3e5;
+		} 
+		fix{ 
+			color: #c7254e;
+			background-color: #f9f2f4;
+		}
+		ex{ 
+			color: green;
+			background-color: #16f71a;
+		}
+		rad { 
+			color: #e9f5e9;
+			background-color: #22d428;
+		}
+	</style>
+</head>
+
+<body>
+<!-- <hr/>  Esto es una raya separadora -->
+<div>
+    <h2>Whatsapp M Pro 13.2</h2>
+    <date> 05/02/2021</date>
+    <br style="clear:both;" />
+</div>
+<p> 
+	<wbase>Base version 2.21.2.6</wbase></br>
+	<new>~</new> Diseño de selector de stickers, formar, emojis al editar una imagen</br>
+	<new>~</new> Ignorar chats archivados</br>
+	<new>~</new> Ocultar chats archivados</br>
+    <new>~</new> Antiguo diseño de chats archivados</br>
+    <new>~</new> Antiguo diseño de lista de contactos bloqueados</br>
+    <fix>~</fix> Corregidos Problema al intentar bloquear un contacto</br>
+	<fix>~</fix> Corregidos Problemas con la traducción de Respuesta automática y Mensajes programados</br>
+</p>
+<hr>
+<div>
+    <h2>Whatsapp M Pro 13.1</h2>
+    <date> 03/02/2021</date>
+    <br style="clear:both;" />
+</div>
+<p> 
+	<wbase>Base version 2.21.2.6</wbase></br>
+	<fix>~</fix>Corregidos Problemas con las respuestas automáticas</br>
+</p>
+<hr>
+<div>
+    <h2>Whatsapp M Pro 13.0</h2>
+    <date>03/02/2021</date>
+    <br style="clear:both;" />
+</div>
+<p> 
+	<wbase>Base version 2.21.2.6</wbase></br>
+	<new>~</new> Límite de eliminar mensajes para todos incrementado a 100 días</br>
+	<new>~</new> ajustes del mod rediseñados</br>
+	<new>~</new> Nuevo estilo de botones de Descargar estado y marcar estados como visto</br>
+	<new>~</new> Mensajes programados</br>
+	<new>~</new> Respuestas automaticas</br>
+	<new>~</new> Ocultar barra de CHATS, ESTADOS, LLAMADAS</br>
+	<new>~</new> agregado boton para entrar a los ajustes del mod desde la pantalla principal</br>
+	<new>~</new> Opcion para ocultar boton de ajustes del mod en la pantalla principal</br>
+	<new>~</new> Opcion para activar/desactivar los chats ocultos</br>
+	<new>~</new> Opcion para hacer texto seleccionable en los chats</br>
+	<new>~</new> Desactivar/activar leer mas</br>
+	<new>~</new> Ocultar fecha y nombre al copiar dos o mas mensajes</br>
+	<new>~</new> Ocultar contactos frecuentes en la seccion de reenviados</br>
+	<new>~</new> Opcion para activar/desactivar sonido de los videos en los estados</br>
+	<new>~</new> activar/desactivar cambio de bocina/altavoz durante la reproduccion de nota de voz</br>
+	<new>~</new> Opcion para administrar almacenamiento en la seccion de cache</br>
+	<ex>~</ex> Ahora el mod notificar automaticamente cuando haya una nueva version disponible</br>
+	<fix>~</fix> Corregidos Errores con la Copia de seguridad y Restauracion</br>
+	<fix>~</fix> Corregidos Errores con la limpieza de la cache</br>
+	<fix>~</fix> Corregidos Errores con los estados</br>
+	<fix>~</fix> Corregidos Problemas con los mods multimedia</br>
+	<new>~</new> Mejoras generales, mas optimizado y otras correcciones de errores</br>
+</p>
+
+</body>
+</html>
