232e41267fa2b159c915b766600c898809347c9c
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-02-16 14:08:52 -0500

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,22 @@
+{
+	"version": "13.4",
+	"download": "http://motriael.com/73GQ",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+		},
+		{
+			"languaje": "pt-BR",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+		}
+	]
+}
