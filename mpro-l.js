422f399c88b339fe8fdb8d4e4ce4aee46e2cfc92
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-05-21 22:54:13 -0500

    Add files via upload

---
+++ mpro-l.js
@@ -0,0 +1,22 @@
+{
+	"version": "15.2",
+	"download": "http://fumacrom.com/HdMt",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+		},
+		{
+			"languaje": "pt-br",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+		}
+	]
+}
