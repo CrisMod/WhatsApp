0f910d85edd435b6b41723c378dc8dd03e6a5b1f
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-06-27 17:19:40 -0500

    Add files via upload

---
+++ wampro.16-d.js
@@ -0,0 +1,22 @@
+{
+	"version": "16.0",
+	"download": "http://fumacrom.com/HdMt",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-es.d.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.d.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.d.html"
+		},
+		{
+			"languaje": "pt-br",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.d.html"
+		}
+	]
+}
